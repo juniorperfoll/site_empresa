dd056be7b20b8ce063472a9b529219a301f308ed
Ademar Perfoll Junior <junior.bsn@gmail.com>
2018-11-29 16:24:03 -0200

    Atualização

--- conectar.php
+++ conectar.php
@@ -1,9 +1,9 @@
 <?php
-  $servidor = "127.0.0.1";
-  $username = "root";
-  $password = "";
+  $servidor = "localhost";
+  $username = "id8062130_empresa";
+  $password = "empresa";
   try {
-    $conn = new PDO("mysql:host=$servidor;dbname=empresa",$username,$password);
+    $conn = new PDO("mysql:host=$servidor;dbname=id8062130_empresa",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     //echo "Conectou com sucesso!";
   } catch(PDOException $erro) {
